62f32387dc3fd9573a23cc3396a13aaa0986328f
nanali126 <156862878+nanali126@users.noreply.github.com>
2024-06-03 16:25:13 +0000

    first setup and some code

---
+++ first.py
@@ -0,0 +1,14 @@
+import os
+import pandas as pd
+import numpy as np
+
+filename = "works-2024-06-03T15-29-17.csv"
+file = os.path.join(os.getcwd(), "data", filename)
+df = pd.read_csv(file, header=0)
+
+df_subset = df[['best_oa_location_landing_page_url', 'grants_funder_display_name', 'grants_award_id']]
+
+file_path = 'data/file.csv'
+df.to_csv(file_path, index=False)
+
+print(df_subset.head())
